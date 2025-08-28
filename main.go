dfca79a70b5c0dfdb86c4700382d1275e4eba652
Naman B Gor <developer.namangor@gmail.com>
2025-08-28 19:22:58 +0530

    Add: Github Action for SDK and Spec Generation

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 )
 
 // @title NBRGLM Auth Platform
-// @version 0.1.0
+// @version 0.0.1
 // @description This is the NBRGLM Auth Platform API documentation.
 // @termsOfService https://nbrglm.com/auth-platform/terms
 
